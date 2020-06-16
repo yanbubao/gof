3b3ec49a7cd42fc48618225f7015142087372fbf
yanzx <yanbubao@163.com>
2020-06-17 00:43:39 +0800

    add prototype

--- src/main/java/april/pattern/prototype/shallow/Client.java
+++ src/main/java/april/pattern/prototype/shallow/Client.java
@@ -8,6 +8,7 @@ import java.util.List;
  */
 public class Client {
 
+
     public static void main(String[] args) throws CloneNotSupportedException {
 
         ConcretePrototype concretePrototype = new ConcretePrototype();
@@ -24,7 +25,7 @@ public class Client {
         System.out.println(concretePrototype.getList() == cloneObject.getList());
 
         // 修改clone对象的属性值，clone的对象和原型对象的引用类型对象是同一块堆，只是将引用clone了
-        cloneObject.getList().add("22");
+        cloneObject.getList().add("33");
         System.out.println(concretePrototype.getList() == cloneObject.getList());
         System.out.println("原型对象：" + concretePrototype);
         System.out.println("clone的对象：" + cloneObject);
