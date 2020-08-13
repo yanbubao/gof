075c498a18b3d91dd0429e59a9754eaf3fc39612
yanzx <yanbubao@163.com>
2020-08-14 01:04:35 +0800

    todo interpreter calculate demo.

---
+++ src/main/java/april/pattern/behavioral/interpreter/calculate/Client.java
@@ -0,0 +1,10 @@
+package april.pattern.behavioral.interpreter.calculate;
+
+/**
+ * @author yanzx
+ */
+public class Client {
+    public static void main(String[] args) {
+        // TODO 解释器模式实现计算器
+    }
+}
