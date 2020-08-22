b4a6c6a1efc83f6e608e953c12f8b5c596522da2
yanzx <yanbubao@163.com>
2020-08-22 21:20:10 +0800

    add observer with callback demo.

---
+++ src/main/java/april/pattern/behavioral/observer/mouse/handler/Mouse.java
@@ -0,0 +1,11 @@
+package april.pattern.behavioral.observer.mouse.handler;
+
+import april.pattern.behavioral.observer.mouse.core.EventContext;
+
+/**
+ * 具体被观察者
+ *
+ * @author yanzx
+ */
+public class Mouse extends EventContext {
+}
